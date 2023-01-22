5f2e285d1a71c120635d9e336e4947f138c83160
Izu7 <emersonxc97@gmail.com>
2023-01-22 12:42:11 -0300

    primer commit

---
+++ src/test/java/com/adoptaPatitas/proyecto/AdoptaPatitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoptaPatitas.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaPatitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
